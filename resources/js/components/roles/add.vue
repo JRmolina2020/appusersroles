b553dd781f77e1b7be8f592825d64e767e347fb8
JRMOLINA2020 <JRMOLINA2020@GMAIL.COM>
2020-06-22 18:08:14 -0500

    sasas2w2s

--- resources/js/components/roles/add.vue
+++ resources/js/components/roles/add.vue
@@ -119,13 +119,46 @@ export default {
         getlist() {
             this.$store.dispatch("Permissionsactions");
         },
-        async add(id) {
+        add(id) {
             this.$validator.validate().then(valid => {
                 if (valid) {
                     if (id) {
-
+                        let url = `${this.urlroles}${id}`;
+                        axios
+                            .put(url, this.form)
+                            .then(response => {
+                                Swal.fire({
+                                    position: "center",
+                                    icon: "success",
+                                    title: `${response.data.message}`,
+                                    showConfirmButton: false,
+                                    timer: 1500
+                                });
+                                $("#model").modal("hide");
+                                this.$store.dispatch("Roleactions");
+                                this.clear();
+                            })
+                            .catch(error => {
+                                console.log(error.response);
+                            });
                     } else {
-                         let response = await axios.post(this.url, this.form);
+                        axios
+                            .post(this.urlroles, this.form)
+                            .then(response => {
+                                Swal.fire({
+                                    position: "center",
+                                    icon: "success",
+                                    title: `${response.data.message}`,
+                                    showConfirmButton: false,
+                                    timer: 1500
+                                });
+                                $("#model").modal("hide");
+                                console.log(this.form);
+                                this.$store.dispatch("Roleactions");
+                            })
+                            .catch(error => {
+                                console.log(error.response);
+                            });
                     }
                 }
             });
